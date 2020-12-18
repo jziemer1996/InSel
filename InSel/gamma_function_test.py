dfe077d7983e57bf8f7b190517f32d4409d85bcd
jziemer1996 <jz199623@gmail.com>
2020-12-18 10:42:03 +0100

    made SLC_import work!

--- InSel/gamma_function_test.py
+++ InSel/gamma_function_test.py
@@ -28,18 +28,25 @@ def extract_files_to_list(path_to_folder, datatype, datascenes_file):
     return new_list
 
 
-def deburst_S1_SLC(path_to_folder, datatype):
-    datascenes_file = path_to_folder + 'datascenes.asc'
+def deburst_S1_SLC(path_to_folder):
+    datascenes_file = path_to_folder + 'datascenes.zipfile_list'
     print(datascenes_file)
     zip_file_list = extract_files_to_list(path_to_folder, datatype=".zip", datascenes_file=datascenes_file)
 
     for file in zip_file_list:
         file_name = file[file.find("S1"):len(file)-4] + ".burst_number_table"
         print("Masterfile is...:" + file)
-        os.system("S1_BURST_tab_from_zipfile " + "- " + file)
+        os.system("S1_BURST_tab_from_zipfile" + " - " + file)
 
         os.replace(file_name, path_to_folder + file_name)
 
 
-def SLC_import(path_to_folder, datatype):
-    print("")
+def SLC_import(path_to_folder):
+    datascenes_file = path_to_folder + 'datascenes.zipfile_list'
+    one_scene_file = path_to_folder + "one_scene.zipfile_list"
+    with open(datascenes_file, "rt") as slc_zip_list:
+        for element in slc_zip_list:
+            with open(one_scene_file, 'w') as f:
+                    f.write(element)
+            os.system("S1_import_SLC_from_zipfiles " + one_scene_file + " " + element[:len(element)-4] + "burst_number_table"
+                      + " - 0 0 . 1")
