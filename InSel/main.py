47e0811aeff1bde8fb3f1841745311d00dae2977
marlinmm <marlinmueller.mm@gmail.com>
2021-01-20 09:42:03 +0100

    cleaned up file

--- InSel/main.py
+++ InSel/main.py
@@ -22,14 +22,14 @@ def main():
 
     dem_dir = download_dir + "DEM/"
 
-    shapefile_path = home_path + "GEO410/Scripts/InSel/shapefiles/augrabies_extent.shp"
+    shapefile_dir = home_path + "GEO410/Scripts/InSel/shapefiles/augrabies_extent.shp"
 
     processing_dir = home_path + "GEO410_data/"
     list_dir = home_path + "GEO410_data/lists/"
     slc_dir = home_path + "GEO410_data/slc/"
     orbit_dir = home_path + "GEO410_data/orbit_files/"
-    shapefile_dir = home_path + "GEO410/Scripts/InSel/shapefiles/thuringia.shp"
 
+    # Settings for data download:
     if not os.path.exists(download_dir):
         os.makedirs(download_dir)
 
@@ -37,36 +37,15 @@ def main():
     password = "8DH5BkEre5kykXG"
     api_url = "https://scihub.copernicus.eu/apihub/"
     start_date = "2020-06-01"
-    end_date = "2020-06-16"
+    end_date = "2020-07-30"
 
-    # s1 = SentinelDownloader(username, password, api_url='https://scihub.copernicus.eu/apihub/')
-    # s1.set_geometries(
-    #     'POLYGON ((13.501756184061247 58.390759025092443,13.617310497771715 58.371827474899703,13.620921570075168 58.27891592167088,13.508978328668151 58.233319081414017,13.382590798047325 58.263723491583974,13.382590798047325 58.263723491583974,13.501756184061247 58.390759025092443))')
-    # s1.set_download_dir('./')  # default is current directory
-    # s1.search('S1A*', 0.8, productType='GRD', sensoroperationalmode='IW')
-    # s1.write_results(type='wget', file='test.sh.neu')  # use wget, urls or json as type
-    # s1.download_all()
-    #
-    #
-    # s1 = SentinelDownloader(username, password, api_url='https://scihub.copernicus.eu/apihub/')
-    # # s1.load_sites('wetlands_v8.shp')
-    # s1.set_geometries(
-    #     'POLYGON ((13.501756184061247 58.390759025092443,13.617310497771715 58.371827474899703,13.620921570075168 58.27891592167088,13.508978328668151 58.233319081414017,13.382590798047325 58.263723491583974,13.382590798047325 58.263723491583974,13.501756184061247 58.390759025092443))')
-    # s1.set_download_dir('./')  # default is current directory
-    #
-    # # set additional directories which contain downloaded scenes.
-    # # A scene is only going to be downloaded if it does not yet exist in either of the data directories or the download directory.
-    # s1.set_data_dir('/path/to/datadir1')
-    # s1.set_data_dir('/path/to/datadir2')
-    #
-    # s1.search('S1A*', 0.8, productType='GRD', sensoroperationalmode='IW')
-    # s1.write_results(file_type='wget', filename='sentinel_api_download.sh')  # use wget, urls or json as type
-    # s1.download_all()
+    # Data download function
+    # sentinel_download.copernicus_download(copernicus_username=username, copernicus_password=password,
+    #                                       download_directory=download_dir, api_url=api_url, satellite="S1A*",
+    #                                       min_overlap=0.1, start_date=start_date, end_date=end_date,
+    #                                       product="SLC", orig_shape=shapefile_dir)
 
-    # sentinel_download.copernicus_download(copernicus_username=username, copernicus_password=password, download_directory=download_dir,
-    #                     api_url=api_url, satellite="S1A*", min_overlap=0.1, start_date=start_date, end_date=end_date,
-    #                     product="SLC", orig_shape=shapefile_dir)
-
+    # GAMMA functions for processing:
     # gamma_function_test.display_slc()
 
     gamma_function_test.deburst_S1_SLC(processing_dir=processing_dir, download_dir=download_dir, list_dir=list_dir)
@@ -76,19 +55,19 @@ def main():
     gamma_function_test.define_precise_orbits(slc_dir=slc_dir, orbit_dir=orbit_dir)
 
     gamma_function_test.multilook(slc_dir=slc_dir)
-    # TODO: hier funktioniert der Übergang zwischen den Funktionen nicht fehlerfrei
+    # TODO: hier funktioniert der Übergang zwischen den Funktionen nicht fehlerfrei <-- Bei mir schon... ;)
 
-    gamma_function_test.create_dem_for_gamma(dem_dir=dem_dir, shapefile_path=shapefile_path)
+    gamma_function_test.create_dem_for_gamma(dem_dir=dem_dir, shapefile_path=shapefile_dir)
 
     gamma_function_test.gc_map(slc_dir=slc_dir, dem_dir=dem_dir)
 
     gamma_function_test.geocode_dem(dem_dir=dem_dir)
 
-    # gamma_function_test.coreg(slc_dir=slc_dir, dem_dir=dem_dir)
+    gamma_function_test.coreg(slc_dir=slc_dir, dem_dir=dem_dir)
 
-    # gamma_function_test.geocode_back(slc_dir=slc_dir, dem_dir=dem_dir)
-    #
-    # gamma_function_test.data2geotiff(dem_dir=dem_dir, slc_dir=slc_dir)
+    gamma_function_test.geocode_back(slc_dir=slc_dir, dem_dir=dem_dir)
+
+    gamma_function_test.data2geotiff(dem_dir=dem_dir, slc_dir=slc_dir)
 
     end_time = datetime.now()
     print("#####################################################")
