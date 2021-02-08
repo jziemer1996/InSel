ace4ca26b4c360f36ff34d760a0d9cf0383f23f7
marlinmm <marlinmueller.mm@gmail.com>
2021-02-08 22:17:54 +0100

    added paths to all variables

--- InSel/gamma_support_functions.py
+++ InSel/gamma_support_functions.py
@@ -1,4 +1,5 @@
 import os
+from user_data import *
 
 
 def create_dem_for_gamma(dem_dir, shapefile_path):
@@ -14,26 +15,26 @@ def create_dem_for_gamma(dem_dir, shapefile_path):
     dem_autocreate(geometry=shape_vector, demType="SRTM 1Sec HGT", outfile=dem_dir + "dem_final.dem", buffer=0.05)
 
 
-def geocode_back(slc_dir, dem_dir):
+def geocode_back():
     """
 
     :param slc_dir:
     :param dem_dir:
     :return:
     """
-    os.system("geocode_back " + slc_dir + "20201025.vv.mli " + "9685 " + dem_dir + "DEM_final_lookup.lut " + slc_dir
-              + "20201025.vv_geocode.mli " + "3290 " + "- 2 0")
+    os.system("geocode_back " + Paths.slc_dir + "20201025.vv.mli " + "9685 " + Paths.dem_dir + "DEM_final_lookup.lut "
+              + Paths.slc_dir + "20201025.vv_geocode.mli " + "3290 " + "- 2 0")
 
 
-def data2geotiff(dem_dir, slc_dir):
+def data2geotiff():
     """
 
     :param dem_dir:
     :param slc_dir:
     :return:
     """
-    os.system("data2geotiff " + dem_dir + "dem_final.dem.par " + slc_dir + "20201025.vv_geocode.mli " + "2 " + slc_dir
-              + "output3.tif")
+    os.system("data2geotiff " + Paths.dem_dir + "dem_final.dem.par " + Paths.slc_dir + "20201025.vv_geocode.mli " + "2 "
+              + Paths.slc_dir + "output3.tif")
 
 
 def display_slc():
