f9627d820d4500a4cdee0f7c95de148c8733d312
jziemer1996 <jz199623@gmail.com>
2021-03-04 11:09:00 +0100

    added comments

--- InSel/main.py
+++ InSel/main.py
@@ -15,32 +15,31 @@ if __name__ == '__main__':
     start_time = datetime.now()
 
 
-    ##### GAMMA functions for processing: #####
+    ##### GAMMA functions for processing #####
 
     def processing(processing_step, swath_flag, polarization, resolution, demType, buffer, clean_flag, bperp_max,
                    delta_T_max, create_rasterstack, stackname):
 
-        # SLC_import(polarization=[Processing.polarization], swath_flag=swath_flag)
-        #
-        # multilook(processing_step, resolution)
-        #
-        # gc_map(processing_step, demType, buffer)
-        #
-        # geocode_dem(processing_step)
+        SLC_import(polarization=[Processing.polarization], swath_flag=swath_flag)
 
-        # coreg(processing_step, clean_flag, bperp_max, delta_T_max, polarization, resolution)
+        multilook(processing_step, resolution)
 
-        # coherence_calc()
+        gc_map(processing_step, demType, buffer)
+
+        geocode_dem(processing_step)
+
+        coreg(processing_step, clean_flag, bperp_max, delta_T_max, polarization, resolution)
+
+        coherence_calc()
 
         geocode_coherence(create_rasterstack, stackname)
 
 
-    ######### Decide, which way to execute ############
-
-    ##### Data download function: #####
+    ##### Data download function #####
     if DownloadParams.download:
         sentinel_download.copernicus_download()
 
+    ##### GAMMA processing function #####
     processing(processing_step=Processing.processing_step, swath_flag=Processing.swath_flag,
                polarization=Processing.polarization, resolution=Processing.resolution, demType=Processing.demType,
                buffer=Processing.buffer, clean_flag=Processing.clean_flag, bperp_max=Processing.bperp_max,
@@ -48,6 +47,7 @@ if __name__ == '__main__':
                stackname=Processing.stackname)
 
     end_time = datetime.now()
+
     print("######### Successfully finished processing! #########")
     print("#####################################################")
     print("processing-time = ", end_time - start_time, "Hr:min:sec")
