ae6e6695505f89dbc5feb3c26050ee7365f3b70f
jziemer1996 <jz199623@gmail.com>
2021-03-05 12:09:51 +0100

    deleted comments

--- InSel/user_data.py
+++ InSel/user_data.py
@@ -12,32 +12,49 @@ def user_data():
     # text
     shapefile_dir = home_path + "GEO410/Scripts/InSel/shapefiles/augrabies_extent.shp"
     point_samples_dir = home_path + "GEO410/Scripts/InSel/shapefiles/points/"
-    # TODO: ORBIT FILES DIRECTORY??????
     orbit_file_dir = home_path + "GEO410_data/orbit_files/"
 
-    # Sentinel-1 download parameters
+    ##### Sentinel-1 download parameters #####
     username = "marlinmm2"
     password = "8DH5BkEre5kykXG"
     api_url = "https://scihub.copernicus.eu/apihub/"
+    # Choose the start date of the download interval
     start_date = "2020-06-01"
+    # Choose the end date of the download interval
     end_date = "2020-07-30"
+    # Choose the satellite, on which the download will be based on (options: "S1A*", "S1B*", "Sentinel-1")
     satellite = "S1A*"
+    # Choose the minimum overlap
     min_overlap = 0.1
+    # Choose the product you want to download (options: "SLC", "GRD")
     product = "SLC"
+    # Do you want to download scenes before processing?
     download = False
 
-    # GAMMA processing parameters
-    processing_step = "multi"
+    ##### GAMMA processing parameters #####
+    # Choose, if single-master or multi-master processing
+    processing_step = "single"
+    # Choose 0 and processing will be based on only one swath (quicker) or 1: processing with all swaths (slower)
     swath_flag = 0
+    # Choose, in which polarization you want to process
     polarization = "vv"
+    # Choose your desired multilook resolution (default = 40)
     resolution = 40
+    # Choose, which DEM type and buffer distance around the investigation area to use for geocoding
     demType = "SRTM 1Sec HGT"
     buffer = 0.05
+    # Choose 1, if temp directories of coregistration should be deleted; if 0: not deleted
     clean_flag = "1"
+    # Choose the maximum value of spatial baseline (in meters) for SBAS processing
     bperp_max = 136
+    # Choose the maximum value of temporal baseline (in days) for SBAS processing
     delta_T_max = 48
+    # Do you want to create a raster stack of geocoded coherence scenes?
     create_rasterstack = False
+    # Specify the name of the rasterscene
+    # NOTE: if name already exists, rename it, otherwise the creation of the stack will fail
     stackname = "coherence_stack.tif"
+    # Do you want to plot the coherence of different land cover classes over time?
     plot_bool = True
 
     return home_path, download_dir, shapefile_dir, point_samples_dir, orbit_file_dir, username, password, api_url, \
@@ -53,6 +70,7 @@ class Paths(object):
     # slc_dir = home_path + "GEO410_data/slc/"
     # dem_dir = home_path + "GEO410_data/DEM/"
 
+    # TODO: Delete my shit
     processing_dir = home_path + "GAMMA_testdata_1/"
     list_dir = home_path + "GAMMA_testdata_1/lists/"
     slc_dir = home_path + "GAMMA_testdata_1/slc_processing/"
